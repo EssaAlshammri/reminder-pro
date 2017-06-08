d530a9f5e79deabafafb05ff179fbf693abf797e
EssaAlshammri <essa.alshammri@ymail.com>
2017-06-09 01:47:42 +0300

    creating App component

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render(){
+        return(
+            <div className='App'>
+                <div className='title'>Reminder Pro </div>
+                <div className='form-inline'>
+                    <div className='form-group'>
+                        <input placeholder='What you wanna do ?' className='form-control'/>
+                    </div>
+                    <button type='button' className='btn btn-success'>Add Reminder</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
